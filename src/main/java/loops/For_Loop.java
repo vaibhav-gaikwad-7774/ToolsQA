f24d13da2eba100a6008bbea0447f433b03c56c9
vaibhav-gaikwad-777 <vaibhavgaikwad7774@gmail.com>
2025-08-29 23:42:19 +0530

    files on 29 aug

---
+++ src/main/java/loops/For_Loop.java
@@ -0,0 +1,16 @@
+package loops;
+
+
+public class For_Loop {
+	public static void main(String[] args) {
+		for(int Increment = 0; Increment<= 5; Increment ++) {
+			System.out.println("Count is = "+ Increment); 
+		}
+		
+		System.out.println();
+		
+	}
+	
+	
+
+}
