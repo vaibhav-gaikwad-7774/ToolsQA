5dca5d675b7f0b5bd594dcbdb72dfb43411d6615
vaibhav-gaikwad-777 <vaibhavgaikwad7774@gmail.com>
2025-09-06 12:41:00 +0530

    new codes

---
+++ src/main/java/Classes/ClassTestExercise.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class ClassTestExercise {
+
+	public static void main(String[] args) {
+		//Use the Car keyword to declare Car Class variable
+		Car Toyota = new Car();;
+
+		Toyota.bLeftHandDrive = true;
+		Toyota.iDoors = 4;
+		Toyota.iGear = 5;
+		Toyota.iHighestSpeed = 200;
+		Toyota.iMake = 2014;
+		Toyota.iTyres = 4;
+		Toyota.sColor = "Black";
+		Toyota.sTransmission = "Manual";
+		Toyota.sModel = "Camry";
+		}
+	}
