fd1cc9e080b4af2c22ed9d512cdaba7c02ae89a4
vaibhav-gaikwad-777 <vaibhavgaikwad7774@gmail.com>
2025-10-07 23:33:56 +0530

    7 oct 2025

---
+++ src/main/java/Example/For_Loop.java
@@ -0,0 +1,32 @@
+package Example;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i<=5; i++) {
+			System.out.println("count is ==>" + i);
+			
+		}
+		
+		//decreament count
+		System.out.println("decreament count");
+		for(int j = 5; j<=0; j--) {
+			System.out.println("count is ==>" + j);
+			
+		}
+		
+		//decreament count
+		
+		System.out.println();
+		for(int k = 0; k<=5; k+=2) {
+			System.out.println("count is ==>" + k);
+			
+		}
+
+	}
+
+	
+
+	
+}
