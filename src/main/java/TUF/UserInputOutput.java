2da7e6ef51854ad0a31e99cec8f85829bebfc025
vaibhav-gaikwad-777 <vaibhavgaikwad7774@gmail.com>
2025-10-08 10:33:20 +0530

    8 oct 2025

--- src/main/java/TUF/UserInputOutput.java
+++ src/main/java/TUF/UserInputOutput.java
@@ -5,27 +5,23 @@ import java.util.Scanner;
 public class UserInputOutput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc  = new Scanner(System.in);
-		System.out.print("Please enter your choice");
-		
-		char ch = sc.next().charAt(0);
-		
-		if (ch >= 'A' && ch<='Z') {
-			
-			System.out.println(0);
-		}
-		
-		if (ch >= 'a' && ch<='z') {
-			
-			System.out.println(1);
-		}
-		else {
-			System.out.println(-1);
-		}
-		sc.close();
-	
+        // Create a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Read a character input from the user
+        char ch = sc.next().charAt(0);
 
-	}
-
+        // Check if the character is an uppercase letter, then Print 1
+        if(ch >= 'A' && ch <= 'Z')
+            System.out.println(1); 
+        // Check if the character is a lowercase letter, then Print 0
+        else if(ch >= 'a' && ch <= 'z')
+            System.out.println(0);
+        // If neither, it's not a letter, then Print -1
+        else 
+            System.out.println(-1);
+        
+        // Close the scanner to prevent resource leak
+        sc.close();
+    }
 }
