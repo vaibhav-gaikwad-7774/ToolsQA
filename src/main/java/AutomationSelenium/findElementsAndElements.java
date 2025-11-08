daa4d99ab3e0f904a4595b00c202afac8b78e257
vaibhav-gaikwad-777 <vaibhavgaikwad7774@gmail.com>
2025-11-08 21:55:40 +0530

    sat 8 nov

---
+++ src/main/java/AutomationSelenium/findElementsAndElements.java
@@ -0,0 +1,23 @@
+package AutomationSelenium;
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+
+
+public class findElementsAndElements {
+	public static void main(String[] args) {
+		WebDriver driver = new SafariDriver();
+		
+		driver.get("https://demoqa.com/text-box/");
+		
+//		List<WebElement> allInputElements = driver.findElement(By.tagName("input"));
+		
+		
+	}
+
+}
